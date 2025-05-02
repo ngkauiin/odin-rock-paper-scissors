17e6666a42e672770042a95528df09715babd97c
kuaiin.v.ng@gmail.com <ngkauiin@gmail.com>
2025-05-02 12:17:20 +1000

    Add eventListener to buttons and make script in HTML defer
    script wasn't defer resulting in the button can't be selected as intented.

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 console.log("hello world")
 
 function getComputerChoice() {
-  // Use Math.random() to generate random number from 0-1
-  // Check if the generated number is <= 1/3, then it is 'rock'
-  // Else if the number is > 1/3 AND <= 2/3, then it is 'paper'
-  // Else if the number is >2/3 AND <= 1, then it is scissors
   let randomNumber = Math.random()
   if (randomNumber <= 1/3) {
     return 'rock'
@@ -16,22 +12,11 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  // Use prompt to get human input (assume always valid)
-  // return and console.log the result
   return prompt("Rock, paper or scissors?")
 }
 
 let humanScore = 0;
 let computerScore = 0;
-
-// Get human choice and computer choice
-// Create a function to use human choice and computer choice as parameters
-// Make human choice lowercase
-// Check if human choices is the same as computer choice then draw
-// Else if human choice is 'rock' && computer choice is 'paper' then computer wins
-// Else human wins
-// Print/console.log the results
-// Increment the score for winner
 
 function playRound(humanChoice, computerChoice) {
   let lowerCasedHumanChoice = humanChoice.toLowerCase();
@@ -64,20 +49,14 @@ function playRound(humanChoice, computerChoice) {
   }
 }
 
-// Create a function that run playRound(..) 5 rounds
-// Use FOR loop to run playRound() 5 times
-// Get human and computer choices
-// After FOR loop ends, check if human score === computer score then alert draw
-// Else if human score is >= computer score then alert human wins
-// Else alert computer wins
+const btns = document.querySelectorAll('button');
+btns.forEach((btn)=> {
+  btn.addEventListener("click", ()=>{
+    console.log(btn.className);
+  })
+})
 
 function playGame() {
-  for (i=0;i<5;i++) {
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-    playRound(humanChoice,computerChoice);
-  }
-  
   if (humanScore === computerScore) {
     alert(`${humanScore}:${computerScore} - Draw!`);
   } else if (humanScore >= computerScore) {
@@ -87,4 +66,4 @@ function playGame() {
   } 
 }
 
-playGame();
+// playGame();
